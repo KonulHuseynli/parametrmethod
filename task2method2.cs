68ba25f324676ca7372e4a4628056323b25928b5
KonulHuseynli <konulrh@code.edu.az>
2022-05-15 23:04:59 +0400

    added newtask file

---
+++ task2method2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 24 };
+            PrintArraySum(12,24);
+           
+        }
+        static int PrintArraySum (int [] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+                Console.WriteLine(sum);
+                if(sum%2==0)
+                {
+                    return sum % 2;
+                } 
+            } return -1;
+        }
+        
+    }
+}
---
+++ task3methodstar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Printstar(5, 5);
+        }
+        static void Printstar(int en, int hundurluk)
+        {
+
+            for (int i = 0; i <= en; i++)
+
+            {
+                for (int j = 0; j < i; j++)
+
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+}
+
